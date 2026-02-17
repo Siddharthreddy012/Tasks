53a4caad91cfb2ea6c3df4a7dbe25e82ac266df2
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2026-02-17 20:21:23 +0530

    task

---
+++ Day_1.py
@@ -0,0 +1,77 @@
+#Task-01
+
+print("Python Programming")
+
+
+#Task-02
+
+name="Siddharth"
+roll_num="25mra04097"
+dob="27-12-2007"
+phn_num=7337567619
+marks=25
+print(name)
+print(roll_num)
+print(dob)
+print(phn_num)
+print(marks)
+
+
+#Task-03
+
+weight=50
+print(weight)
+print(type(weight))
+cost_soap=20.32
+print(cost_soap)
+print(type(cost_soap))
+clg_name="MITS"
+print(clg_name)
+print(type(clg_name))
+wt_1=70
+wt_2=50
+res=wt_1>wt_2 or wt_1<wt_2
+print(res)
+print(type(res))
+
+
+#Task-04
+
+#indexing
+word="Thats one small step for a man, a giant leap for mankind"
+
+# printing first occurrence of letter 'o' in forward 
+print(word[6])
+
+# printing fourth occurrence of letter 'a' in forward 
+print(word[28])
+
+# printing letter 'g' in backward
+print(word[-22])
+
+#printing second occurrence of letter 's' in backward
+print(word[-46])
+
+#slicing
+word="Thats one small step for a man, a giant leap for mankind"
+
+#printing one in forward
+print(word[6:9:1])
+
+#printing leap in forward
+print(word[40:44:1])
+
+#printing nam in backword
+print(word[-27:-30:-1])
+
+#printing llams in backward
+print(word[-42:-47:-1])
+
+#reversing the word using slicing 
+word1="siddharth"
+print(word1[-1::-1])
+
+word2="dheekshith"
+print(word2[-1::-1])
+
+
